efd08352390591918656cbf76a17bf9e1803f452
Adrien Millot <@UF606AMI>
2024-02-15 14:12:00 +0100

    💄 New Button + Auto Position base on Tab

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import Image
 
+
+#-------------POur PYINSTALLER--------------------#
+import os
+import sys
+#Commande pour créér un .exe : pyinstaller main.spec
+
 #-------------------INIT-----------#
 
 Chemin=''
@@ -17,6 +23,31 @@ chemin_init="C:\\Users\Adrie\Desktop\Convertir PNG EN PDF Python"
 chemin_final=''
 Images_Multiple=[]
 IMG_Mult=[]
+
+#Gestion chemin fichier #Format adapté pour pyinstaller : 
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+#-----------------------------Calcul position boutons 
+def CalculPositionInitialeBoutonsDeControl():
+    global Tableau_width,Boutons_Controle, Btn_controle_width, Btn_controle_height,Space_Between_Btn,Btn_controle_x_init, Tableau_x_position
+    #Tous les boutons vont être collés et centré sur le tableau
+    NbBtn = len(Boutons_Controle)
+    EspacePrisParLesBoutons = NbBtn * (Btn_controle_width+Space_Between_Btn)
+    freeSpace = Tableau_width - (EspacePrisParLesBoutons)
+    if(freeSpace>0):
+        Btn_controle_x_init = Tableau_x_position + (freeSpace / 2)
+    else:
+        debord = EspacePrisParLesBoutons - Tableau_width
+        Btn_controle_x_init = Tableau_x_position - (debord / 2)
+
+    return Btn_controle_x_init
 
 def choosefile():
     global Chemin
@@ -104,36 +135,58 @@ gui(root) # Selection du fichier csv
 Tableau_x_position = 50 ; Tableau_y_position = 220 
 Tableau_width = 600 ; Tableau_Height = 250
 
+#Bouton
+Btn_controle_width = 50 ; Btn_controle_height = 50 ; Space_Between_Btn = 10
+Btn_controle_x_init = 0 ; Btn_controle_y_init = Tableau_y_position - Btn_controle_height - 30
+policeSize = 10
+
+#image Btn Controle 
+ImageReducer =  1
 
 #----------------------------------------
+#Nom Bouton, Texte, Image, Fonction
 
-Bouton_Emplacement_Sauvegarde = tk.Button(tab1, text="Dossier de Sauvegarde", command=Save_Path)
-Bouton_Emplacement_Sauvegarde.place(x=50, y=25, width=200, height=40)
+img_SelectFile = PhotoImage(file=resource_path("Pictures/AddFile.png"))
+img_Reset = PhotoImage(file=resource_path("Pictures/Reset.png"))
+img_Convert = PhotoImage(file=resource_path("Pictures/ConvertInPdf.png"))
+img_Exit = PhotoImage(file=resource_path("Pictures/Exit.png"))
 
-Bouton_Selec_image = tk.Button(tab1, text="Selectionner fichier", command=choosefile)
-Bouton_Selec_image.place(x=150, y=160, width=400, height=50)
+Btn_SelectFile = tk.Button(tab1) ; Btn_Reset = tk.Button(tab1) ; 
+Btn_Convertir = tk.Button(tab1) ; Btn_Quitter = tk.Button(tab1) ; 
 
-Bouton_Validation = tk.Button(tab1, text="Convertir en Pdf", command=Convertir_pdf)
-Bouton_Validation.place(x=150, y=500, width=400, height=50)
+Boutons_Controle = [
+    [Btn_SelectFile, "Add file",img_SelectFile, choosefile ],
+    [Btn_Reset, "Reset",img_Reset, RESET],
+    [Btn_Convertir, "Convertir",img_Convert, Convertir_pdf],
+    [Btn_Quitter, "Quitter",img_Exit, root.destroy],
+]
 
-Bouton_Reset= Button(tab1, text="Reset selection", command=RESET)
-Bouton_Reset.place(x=150, y=575, width=200, height=50)
+# Boucle placement des bouttons
+CalculPositionInitialeBoutonsDeControl()
 
-Bouton_Quitter = Button(tab1, text="Quitter", command=root.destroy)
-Bouton_Quitter.place(x=350, y=575, width=200, height=50)
+for i in range(0,len(Boutons_Controle)):
+    Boutons_Controle[i][2] = Boutons_Controle[i][2].subsample(ImageReducer, ImageReducer) #Réduction de la taille de l'image
+    Boutons_Controle[i][0].configure( width=Btn_controle_width, height= Btn_controle_height, font=("Helvetica", policeSize),image=Boutons_Controle[i][2], command=Boutons_Controle[i][3], text = Boutons_Controle[i][1],compound=tk.TOP)
+    Boutons_Controle[i][0].place(x=Btn_controle_x_init, y=Btn_controle_y_init)
+    Btn_controle_x_init = Btn_controle_x_init + Btn_controle_width + Space_Between_Btn
+    Boutons_Controle[i].append(Btn_controle_x_init) #Sauvegarde de la valeur x du bouton à la fin de la liste
+    Boutons_Controle[i].append(Btn_controle_y_init) #Sauvegarde de la valeur y du bouton à la fin de la liste
 
 
-# CHemin de la sauvegarde
-lbl1 = Label(tab1, text='Veuillez selectionner un dossier', width=10)
-lbl1.place(x=250, y=25, width=400, height=40)
+# Bouton_Emplacement_Sauvegarde = tk.Button(tab1, text="Dossier de Sauvegarde", command=Save_Path)
+# Bouton_Emplacement_Sauvegarde.place(x=50, y=25, width=200, height=40)
 
-#Nom fichier final
-lbl2 = Label(tab1, text="Nom du fichier pdf créé : ", width=10)
-lbl2.place(x=50, y=100, width=200, height=30)
-entry2 = Entry(tab1, text="")
-Nom_fichier_init = "Nom du fichier"
-entry2.insert(0, Nom_fichier_init)
-entry2.place(x=250, y=100, width=300, height=30)
+# # CHemin de la sauvegarde
+# lbl1 = Label(tab1, text='Veuillez selectionner un dossier', width=10)
+# lbl1.place(x=250, y=25, width=400, height=40)
+
+# #Nom fichier final
+# lbl2 = Label(tab1, text="Nom du fichier pdf créé : ", width=10)
+# lbl2.place(x=50, y=100, width=200, height=30)
+# entry2 = Entry(tab1, text="")
+# Nom_fichier_init = "Nom du fichier"
+# entry2.insert(0, Nom_fichier_init)
+# entry2.place(x=250, y=100, width=300, height=30)
 
 #tableau
 tableau = ttk.Treeview(tab1, columns=('Position', 'Fichier','Chemin'))
