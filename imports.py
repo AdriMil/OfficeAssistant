2df6e73ec18e94db65f9730d283929863620fe8c
AdriMil <adrien.millot@yahoo.fr>
2024-03-17 13:02:47 +0100

    ♻️ Refacto Code Structure

---
+++ imports.py
@@ -0,0 +1,19 @@
+#-------------Pour_IG--------------------#
+from tkinter import filedialog
+from tkinter import *
+from tkinter import messagebox
+from tkinter import simpledialog
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image
+from pillow_heif import register_heif_opener
+
+# from PicturesConversion import process_images
+from UpdatePosition import *
+
+#-------------POur PYINSTALLER--------------------#
+import os
+import sys
+#Commande pour créér un .exe : pyinstaller main.spec
+
+
