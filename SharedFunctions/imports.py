4585e6fdb612964a732c8c9e244827c6c594ad7c
AdriMil <adrien.millot@yahoo.fr>
2024-05-04 23:19:06 +0200

    ♻️ Renaming Variable Refacto

--- SharedFunctions/imports.py
+++ SharedFunctions/imports.py
@@ -21,9 +21,9 @@ import sys
 
 #------------Design UI VARIABLE SPACE-------------#
 #Tableau Tab1:
-Tableau_x_position = 50 ; Tableau_y_position = 200
-Tableau_width = 600 ; Tableau_Height = 250
-Tab1Tableau = [Tableau_x_position,Tableau_y_position,Tableau_width,Tableau_Height]
+Table_x_Position = 50 ; Table_y_Position = 200
+Table_Width = 600 ; Table_Height = 250
+Tab1Table = [Table_x_Position,Table_y_Position,Table_Width,Table_Height]
 
 #Fenetre d'affichage Tab2
 Tab2DisplayWindow_x_position = 50 ; Tab2DisplayWindow_y_position = 100
@@ -32,25 +32,25 @@ Tab2Canvas = [Tab2DisplayWindow_x_position,Tab2DisplayWindow_y_position,Tab2Disp
 
 
 #Bouton Controle Paramètre
-Btn_controle_width = 50 ; Btn_controle_height = 50 ; Space_Between_Btn = 10
-Btn_controle_x_init = 0 ; Btn_controle_y_init = Tableau_y_position - Btn_controle_height - 30
-window_height = Tableau_Height + Btn_controle_y_init + Tableau_y_position
-policeSize = 10
+Control_Button_Width = 50 ; Control_Button_Height = 50 ; Space_Between_Button = 10
+Control_Button_Init_x_Position = 0 ; Control_Button_Init_y_Position = Table_y_Position - Control_Button_Height - 30
+Window_Height = Table_Height + Control_Button_Init_y_Position + Table_y_Position
+Police_Size = 10
 
 #Bouton Fleches Tab 1 PAramètres
-Btn_fleche_width = 25 ; Btn_fleche_height = 25 ; Space_Between_Btn_fleche = 10
-Btn_fleche_y_init = Tableau_y_position + Tableau_Height + 10
+Arrows_Buttons_Width = 25 ; Arrows_Buttons_Height = 25 ; Space_Between_Arrows_Buttons = 10
+Arrows_Buttons_Init_y_Position = Table_y_Position + Table_Height + 10
 
 #Bouton Zoom Tab 2 PAramètres
-Btn_zoom_width = 25 ; Btn_zoom_height = 25 ; Space_Between_Btn_zoom = 10
-Btn_zoom_y_init = Tab2DisplayWindow_y_position + Tab2DisplayWindow_Height + 10
+Zoom_Buttons_Width = 25 ; Zoom_Buttons_Height = 25 ; Space_Between_Zoom_Buttons = 10
+Zoom_Buttons_Init_y_Position = Tab2DisplayWindow_y_position + Tab2DisplayWindow_Height + 10
 
 #image Btn Controle
-ImageReducer =  1
+Picture_Reducer_Value =  1
 
 
 #Gestion chemin fichier #Format adapté pour pyinstaller :
-def resource_path(relative_path):
+def Ressource_Path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
@@ -62,24 +62,24 @@ def resource_path(relative_path):
 
 
 #---------------Calcul de la position des boutons ----------------------#
-def CalculPositionInitialeBoutonsDeControl(Liste_Boutons_De_Control,TableForYourTab,offset):
+def ControlsButtonsInitPositionCalculation(Liste_Boutons_De_Control,TableForYourTab,offset):
     
-    global  Btn_controle_width, Btn_controle_height,Space_Between_Btn,Btn_controle_x_init,Btn_controle_y_init,window_width
+    global  Control_Button_Width, Control_Button_Height,Space_Between_Button,Control_Button_Init_x_Position,Control_Button_Init_y_Position,Window_Width
     #Tous les boutons vont être collés et centré sur le tableau
     NbBtn = len(Liste_Boutons_De_Control)
-    EspacePrisParLesBoutons = NbBtn * (Btn_controle_width+Space_Between_Btn)
+    EspacePrisParLesBoutons = NbBtn * (Control_Button_Width+Space_Between_Button)
     freeSpace = TableForYourTab[2] - (EspacePrisParLesBoutons)
     if(freeSpace>0):
-        Btn_controle_x_init = TableForYourTab[0] + (freeSpace / 2)
+        Control_Button_Init_x_Position = TableForYourTab[0] + (freeSpace / 2)
 
-        window_width = TableForYourTab[2] + TableForYourTab[0] *2
+        Window_Width = TableForYourTab[2] + TableForYourTab[0] *2
 
     else:
         debord = EspacePrisParLesBoutons - TableForYourTab[2]
-        Btn_controle_x_init = TableForYourTab[0] - (debord / 2)
-        window_width = TableForYourTab[2] + debord
+        Control_Button_Init_x_Position = TableForYourTab[0] - (debord / 2)
+        Window_Width = TableForYourTab[2] + debord
 
-    # Calcul de Btn_controle_y_init
-    Btn_controle_y_init = TableForYourTab[1] - Btn_controle_height - offset
+    # Calcul de Control_Button_Init_y_Position
+    Control_Button_Init_y_Position = TableForYourTab[1] - Control_Button_Height - offset
 
-    return Btn_controle_x_init,Btn_controle_y_init
+    return Control_Button_Init_x_Position,Control_Button_Init_y_Position
