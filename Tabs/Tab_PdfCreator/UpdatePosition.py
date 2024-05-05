c034c569a23c6651f71857c1004b2c73df1ac165
AdriMil <adrien.millot@yahoo.fr>
2024-05-05 12:04:45 +0200

    ♻️ Renamming variables

--- Tabs/Tab_PdfCreator/UpdatePosition.py
+++ Tabs/Tab_PdfCreator/UpdatePosition.py
@@ -1,29 +1,26 @@
-# All_data_in_tableau = [[1, 'ADRIEN MILLOT.png', 'C:/Users/Adrie/Pictures/ADRIEN MILLOT.png'], [2, 'Retouche.png', 'C:/Users/Adrie/Pictures/Retouche.png'], [3, 'Screenshot_2017-07-23-20-05-02.png', 'C:/Users/Adrie/Pictures/Screenshot_2017-07-23-20-05-02.png']]
-# print(All_data_in_tableau)
-
-def ChangePlaceUp(All_data_in_tableau,index):
+def ChangePlaceUp(All_Data_In_Table,index):
     if(index!=0):
-        Sauvegarde_temporaire_element = All_data_in_tableau[index-1]
-        All_data_in_tableau[index-1] = All_data_in_tableau[index-2]
-        All_data_in_tableau[index-2] = Sauvegarde_temporaire_element
+        Store_Value_Temporary = All_Data_In_Table[index-1]
+        All_Data_In_Table[index-1] = All_Data_In_Table[index-2]
+        All_Data_In_Table[index-2] = Store_Value_Temporary
 
-        Sauvegarde_Numero_temporaire = All_data_in_tableau[index-1][0]
-        All_data_in_tableau[index-1][0] = All_data_in_tableau[index-2][0]
-        All_data_in_tableau[index-2][0] = Sauvegarde_Numero_temporaire
+        Store_Number_Temporary = All_Data_In_Table[index-1][0]
+        All_Data_In_Table[index-1][0] = All_Data_In_Table[index-2][0]
+        All_Data_In_Table[index-2][0] = Store_Number_Temporary
 
-def ChangePlaceDown(All_data_in_tableau,index):
-    if(index!=len(All_data_in_tableau)):
-        Sauvegarde_temporaire_element = All_data_in_tableau[index-1]
-        All_data_in_tableau[index-1] = All_data_in_tableau[index]
-        All_data_in_tableau[index] = Sauvegarde_temporaire_element
+def ChangePlaceDown(All_Data_In_Table,index):
+    if(index!=len(All_Data_In_Table)):
+        Store_Value_Temporary = All_Data_In_Table[index-1]
+        All_Data_In_Table[index-1] = All_Data_In_Table[index]
+        All_Data_In_Table[index] = Store_Value_Temporary
 
-        Sauvegarde_Numero_temporaire = All_data_in_tableau[index-1][0]
-        All_data_in_tableau[index-1][0] = All_data_in_tableau[index][0]
-        All_data_in_tableau[index][0] = Sauvegarde_Numero_temporaire
+        Store_Number_Temporary = All_Data_In_Table[index-1][0]
+        All_Data_In_Table[index-1][0] = All_Data_In_Table[index][0]
+        All_Data_In_Table[index][0] = Store_Number_Temporary
 
-def DeleteSelectedLine(All_data_in_tableau, index):
-    All_data_in_tableau.pop(index)
-    print(All_data_in_tableau)
-    for i in range(0,len(All_data_in_tableau)):
-        All_data_in_tableau[i][0] = i + 1
+def DeleteSelectedLine(All_Data_In_Table, index):
+    All_Data_In_Table.pop(index)
+    print(All_Data_In_Table)
+    for i in range(0,len(All_Data_In_Table)):
+        All_Data_In_Table[i][0] = i + 1
 
